21ca9fcc642b5ae1465d3db1f6e06e8dfd1a3a9b
Karl OELSCHLAGER <oelschlager.karl@gmail.com>
2025-02-27 18:57:57 +0100

    Add absolute coordinates for horizontal axe on map.

--- project/grid/src/map.rs
+++ project/grid/src/map.rs
@@ -30,16 +30,10 @@ impl Map {
             grid: initial_view.clone(),
         };
         log_info!("Player has spawned");
-        return map;
+        map
     }
 
     pub fn merge_radar_view(&mut self, new_view: &Vec<Vec<String>>, direction: Direction) {
-        if self.grid.is_empty() {
-            self.grid = new_view.clone();
-            self.position = (new_view.len() as isize / 2, new_view[0].len() as isize / 2);
-            return;
-        }
-
         let (dr, dc) = match direction {
             Direction::North => (-2, 0),
             Direction::South => (2, 0),
@@ -47,17 +41,19 @@ impl Map {
             Direction::West => (0, -2),
         };
 
-        let new_position = (self.position.0 + dr, self.position.1 + dc);
+        let candidate = (self.position.0 + dr, self.position.1 + dc);
 
-        self.grid = Map::merge_radar_view_to_map_grid(&self.grid, new_view, new_position);
-        self.position = new_position;
+        let (new_grid, effective_position) =
+            Map::merge_radar_view_to_map_grid(&self.grid, new_view, candidate);
+        self.grid = new_grid;
+        self.position = effective_position;
     }
 
     fn merge_radar_view_to_map_grid(
         saved: &Vec<Vec<String>>,
         new_view: &Vec<Vec<String>>,
         merge_center: (isize, isize),
-    ) -> Vec<Vec<String>> {
+    ) -> (Vec<Vec<String>>, (isize, isize)) {
         let view_size: usize = new_view.len();
         let half: usize = view_size / 2;
 
@@ -102,59 +98,92 @@ impl Map {
             }
         }
 
-        for i in 0..merged.len() {
-            for j in 0..merged[i].len() {
-                if merged[i][j] == "P" {
-                    merged[i][j] = String::from(" ");
+        for row in merged.iter_mut() {
+            for cell in row.iter_mut() {
+                if cell == "P" {
+                    *cell = String::from(" ");
                 }
             }
         }
 
-        return merged;
+        let effective_position = (merge_center.0 - overall_top, merge_center.1 - overall_left);
+
+        (merged, effective_position)
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use std::string;
+
     use super::*;
     use shared::utils::{print_string_matrix, string_to_strings};
 
     #[test]
     fn test_merge_radar_views_with_directions() {
+        // Spawns.
         let radar_1: Vec<Vec<String>> = vec![
             string_to_strings("#######"),
             string_to_strings("#######"),
-            string_to_strings("##•-•-•"),
-            string_to_strings("##|P   "),
-            string_to_strings("##• •-•"),
+            string_to_strings("•-•-•-•"),
+            string_to_strings("   P   "),
+            string_to_strings("•-• •-•"),
             string_to_strings("##|  A "),
             string_to_strings("##•-•-•"),
         ];
-        print_string_matrix("radar 1", &radar_1);
 
+        print_string_matrix("radar view 1", &radar_1);
+        let mut map = Map::new(&radar_1);
+        print_string_matrix("map + radar view 1", &map.grid);
+        assert_eq!(map.position, (3, 3));
+
+        // Moves West.
         let radar_2: Vec<Vec<String>> = vec![
+            string_to_strings("#######"),
+            string_to_strings("#######"),
+            string_to_strings("•-•-•-•"),
+            string_to_strings("   P   "),
+            string_to_strings("  •-• •"),
+            string_to_strings("  ###  "),
+            string_to_strings("  ###-•"),
+        ];
+        print_string_matrix("radar view 2", &radar_2);
+        map.merge_radar_view(&radar_2, Direction::West);
+        print_string_matrix("map + radar view 2", &map.grid);
+        assert_eq!(map.position, (3, 3));
+
+        // Moves East.
+        let radar_3: Vec<Vec<String>> = vec![
             string_to_strings("##### •"),
-            string_to_strings("#####M|"),
+            string_to_strings("##### |"),
             string_to_strings("•-•-• •"),
-            string_to_strings("|  P  |"),
+            string_to_strings("   P  |"),
             string_to_strings("• •-• •"),
             string_to_strings("|  A   "),
             string_to_strings("•-•-•-•"),
         ];
-        print_string_matrix("radar 2", &radar_2);
+        print_string_matrix("radar view 3", &radar_3);
+        map.merge_radar_view(&radar_3, Direction::East);
+        print_string_matrix("map + radar view 3", &map.grid);
+        assert_eq!(map.position, (3, 5));
 
-        let radar_3: Vec<Vec<String>> = vec![
+        // Moves East.
+        let radar_4: Vec<Vec<String>> = vec![
             string_to_strings("##• •##"),
-            string_to_strings("##|M|##"),
+            string_to_strings("##| |##"),
             string_to_strings("•-• •##"),
             string_to_strings("   P|##"),
             string_to_strings("•-• •##"),
             string_to_strings(" A   ##"),
             string_to_strings("•-•-•-•"),
         ];
-        print_string_matrix("radar 3", &radar_3);
+        print_string_matrix("radar view 4", &radar_4);
+        map.merge_radar_view(&radar_4, Direction::East);
+        print_string_matrix("map + radar view 4", &map.grid);
+        assert_eq!(map.position, (3, 7));
 
-        let radar_4: Vec<Vec<String>> = vec![
+        // Moves South.
+        let radar_5: Vec<Vec<String>> = vec![
             string_to_strings("•-• •##"),
             string_to_strings("    |##"),
             string_to_strings("•-• •-•"),
@@ -163,31 +192,72 @@ mod tests {
             string_to_strings("#######"),
             string_to_strings("#######"),
         ];
-        print_string_matrix("radar 4", &radar_4);
+        print_string_matrix("radar view 5", &radar_5);
+        map.merge_radar_view(&radar_5, Direction::South);
+        print_string_matrix("map + radar view 5", &map.grid);
+        assert_eq!(map.position, (5, 7));
 
-        let expected: Vec<Vec<String>> = vec![
+        // Moves North.
+        let radar_6: Vec<Vec<String>> = vec![
+            string_to_strings("##• •##"),
+            string_to_strings("##| |##"),
+            string_to_strings("•-• •##"),
+            string_to_strings("   P|##"),
+            string_to_strings("•-• •##"),
+            string_to_strings(" A   ##"),
+            string_to_strings("•-•-•-•"),
+        ];
+        print_string_matrix("radar view 6", &radar_6);
+        map.merge_radar_view(&radar_6, Direction::North);
+        print_string_matrix("map + radar view 6", &map.grid);
+        assert_eq!(map.position, (3, 7));
+
+        // Moves North.
+        let radar_7: Vec<Vec<String>> = vec![
+            string_to_strings("•-• •-•"),
+            string_to_strings("|    A|"),
+            string_to_strings("##• •##"),
+            string_to_strings("##|P|##"),
+            string_to_strings("•-• •##"),
+            string_to_strings("    |##"),
+            string_to_strings("•-• •##"),
+        ];
+        print_string_matrix("radar view 7", &radar_7);
+        map.merge_radar_view(&radar_7, Direction::North);
+        print_string_matrix("map + radar view 7", &map.grid);
+        assert_eq!(map.position, (3, 7));
+
+        // Moves South.
+        let radar_8: Vec<Vec<String>> = vec![
+            string_to_strings("##• •##"),
+            string_to_strings("##| |##"),
+            string_to_strings("•-• •##"),
+            string_to_strings("   P|##"),
+            string_to_strings("•-• •##"),
+            string_to_strings(" A   ##"),
+            string_to_strings("•-•-•-•"),
+        ];
+        print_string_matrix("radar view 8", &radar_8);
+        map.merge_radar_view(&radar_8, Direction::North);
+        print_string_matrix("map + radar view 8", &map.grid);
+        assert_eq!(map.position, (5, 7));
+
+        let expected_final_grid: Vec<Vec<String>> = vec![
+            string_to_strings("####•-• •-•"),
+            string_to_strings("####|    A|"),
             string_to_strings("######• •##"),
-            string_to_strings("######|M|##"),
-            string_to_strings("##•-•-• •##"),
-            string_to_strings("##|     |##"),
-            string_to_strings("##• •-• •-•"),
-            string_to_strings("##|  A     "),
-            string_to_strings("##•-•-•-•-•"),
+            string_to_strings("######| |##"),
+            string_to_strings("•-•-•-• •##"),
+            string_to_strings("        |##"),
+            string_to_strings("• • •-• •-•"),
+            string_to_strings("| |  A     "),
+            string_to_strings("• •-•-•-•-•"),
             string_to_strings("###########"),
             string_to_strings("###########"),
         ];
 
-        let mut map = Map::new(&radar_1);
-        print_string_matrix("saved after radar 1", &map.grid);
-        map.merge_radar_view(&radar_2, Direction::East);
-        print_string_matrix("saved after radar 2", &map.grid);
-        map.merge_radar_view(&radar_3, Direction::East);
-        print_string_matrix("saved after radar 3", &map.grid);
-        map.merge_radar_view(&radar_4, Direction::South);
-        print_string_matrix("saved after radar 4", &map.grid);
+        print_string_matrix("expected final grid", &expected_final_grid);
 
-        print_string_matrix("expected final grid", &expected);
-
-        assert_eq!(map.grid, expected);
+        assert_eq!(map.grid, expected_final_grid);
     }
 }
